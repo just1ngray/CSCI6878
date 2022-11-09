eb5b105d147291be807d85969167dbed54751cfb
just1ngray <just1ngray@outlook.com>
2022-11-09 00:26:21 -0400

    gather contributors

--- contributors.py
+++ contributors.py
@@ -1,6 +1,7 @@
 import os
 import re
 import subprocess
+import concurrent.futures
 from pathlib import Path
 
 from tqdm import tqdm
@@ -11,25 +12,34 @@ from db import db
 contributor = re.compile(r"^\s*(?P<ncommits>\d+)\s+(?P<username>[^<]*)\s+"
                          r"<(?P<email>[^>]*)>\s*$")
 
-def fetch_contributors_sync(owner: str,
-                            project: str,
-                            where: Path
-                           ) -> dict[str, int]:
+def fetch_contributors(owner: str,
+                       project: str,
+                       where: Path
+                      ) -> dict[str, int]:
+    """
+    Fetches the contributor list of a given repository by minimally cloning,
+    and then using git command to summarize commits.
+    """
     where = where.absolute()
     where.mkdir(parents=True, exist_ok=True)
-    os.chdir(where)
 
     clone_into = where.joinpath(project)
     if not clone_into.exists():
-        cmd = f"git clone --filter=tree:0 " \
+        tqdm.write("Cloning into "
+                + clone_into.relative_to(os.getcwd()).as_posix())
+        cmd = f"cd '{where.as_posix()}' && git clone --filter=tree:0 " \
               f"https://github.com/{owner}/{project}.git"
-        subprocess.check_output(cmd.split(" "), stderr=subprocess.DEVNULL)
+        subprocess.check_output(cmd,
+                                stderr=subprocess.DEVNULL,
+                                shell=True
+                               )
 
-    os.chdir(clone_into)
-    cmd = "git shortlog --numbered --summary --email"
-    out = subprocess.check_output(cmd.split(" "),
+    cmd = f"cd '{clone_into.as_posix()}' && " \
+           "git shortlog --numbered --summary --email"
+    out = subprocess.check_output(cmd,
                                   encoding="utf-8",
-                                  stderr=subprocess.DEVNULL
+                                  stderr=subprocess.DEVNULL,
+                                  shell=True
                                  )
 
     contributors: dict[str, int] = {}
@@ -41,8 +51,31 @@ def fetch_contributors_sync(owner: str,
     return contributors
 
 
+def insert_contributors(rank: int, contributors: dict[str, int]):
+    """
+    Inserts a list of contributors into the database relating to the
+    repository of a given rank.
+    """
+    db().executemany("""
+        INSERT INTO contributors (rank, email, commits)
+        VALUES (?, ?, ?);
+    """, [
+        (rank, email, ncommits)
+        for (email, ncommits) in contributors.items()
+    ])
+    db().execute("COMMIT;")
+
+
+def wrapper(args: tuple[Path, int, str, str]):
+    "a small wrapper function for main"
+    tmp, rank, owner, project = args
+    return rank, fetch_contributors(owner, project, tmp / owner)
+
 
 def main():
+    tmp = Path("tmp")
+    tmp.mkdir(parents=True, exist_ok=True)
+
     db().execute("""
         SELECT rank, owner, project
         FROM repos
@@ -51,21 +84,14 @@ def main():
             FROM contributors
         );
     """)
-    tmp = Path("tmp")
-    tmp.mkdir(parents=True, exist_ok=True)
 
-    for rank, owner, project in tqdm(db().fetchall()):
-        directory = tmp / owner
-        contributors = fetch_contributors_sync(owner, project, directory)
-
-        db().executemany("""
-            INSERT INTO contributors (rank, email, commits)
-            VALUES (?, ?, ?);
-        """, [
-            (rank, email, ncommits)
-            for (email, ncommits) in contributors.items()
-        ])
-        db().execute("COMMIT;")
+    with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
+        futures = [executor.submit(wrapper, (tmp, *row))
+                   for row in db().fetchall()]
+        for future in tqdm(concurrent.futures.as_completed(futures),
+                           total=len(futures)):
+            rank, contributors = future.result()
+            insert_contributors(rank, contributors)
 
 
 if __name__ == "__main__":
